4b67798726ab5617e81b913eb3a4287d1bdd0b37
Sergi <sergi.bonell.7e4@itb.cat>
2021-04-19 15:35:00 +0200

    Added 537 "Movilidad sostenible"

--- src/E537.java
+++ src/E537.java
@@ -6,31 +6,83 @@ public class E537 {
         Scanner lector = new Scanner(System.in);
         while(lector.hasNextLine()){
             int nPunts = lector.nextInt(), nLinies = lector.nextInt();
-            ArrayList<Integer> possible = new ArrayList<Integer>();
-            possible.add(1);
-            ArrayList<Integer> linies = new ArrayList<>();
-            for(int i = 0; i<nLinies*2; i++){
-                linies.add(lector.nextInt());
-            }
-            boolean afegit;
-            do{
-                afegit = false;
-                for(int i = 0; i<linies.size(); i+=2){
-                    if(possible.contains(linies.get(i)) && !possible.contains(linies.get(i+1))){
-                        possible.add(linies.get(i+1));
-                        afegit = true;
-                    }
-                    else if(possible.contains(linies.get(i+1)) && !possible.contains(linies.get(i))){
-                        possible.add(linies.get(i));
-                        afegit = true;
+            boolean[][] matriu = new boolean[nPunts][nPunts];
+            ArrayList<Integer> camins = new ArrayList<>();
+            camins.add(1);
+
+            if(nLinies > 0) {
+                for (int i = 0; i < nLinies; i++) {
+                    int x = lector.nextInt() - 1, y = lector.nextInt() - 1;
+                    if(x<nPunts && y<nPunts){
+                        matriu[x][y] = true;
+                        matriu[y][x] = true;
                     }
                 }
-            }while (afegit);
 
-            if(nPunts == possible.size())
+                for (int i = 0; i < camins.size(); i++) {
+                    for (int j = 0; j < nPunts; j++) {
+                        if (matriu[i][j] && !camins.contains(j + 1))
+                            camins.add(j + 1);
+                    }
+                }
+            }
+
+            if(nPunts == camins.size())
                 System.out.println("BICI");
             else
                 System.out.println("A PIE");
         }
     }
 }
+
+/*
+1 2
+1 3
+1 4
+1 5
+1 6
+1 7
+1 8
+1 9
+1 10
+1 11
+1 12
+1 13
+1 14
+1 15
+1 16
+1 17
+1 18
+1 19
+1 20
+1 21
+1 22
+1 23
+1 24
+1 25
+1 26
+1 27
+1 28
+1 29
+1 30
+1 31
+1 32
+1 33
+1 34
+1 35
+1 36
+1 37
+1 38
+1 39
+1 40
+1 41
+1 42
+1 43
+1 44
+1 45
+1 46
+1 47
+1 48
+1 49
+1 50
+ */
